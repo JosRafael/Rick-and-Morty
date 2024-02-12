82a985e5c293b081a38cfce6b9e6d70a6dff72be
José Rafael <rafaelmatiashue@gmail.com>
2024-02-12 10:39:13 -0300

    get all character

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const characterId= document.getElementById('characterId');
+const characterId = document.getElementById('characterId');
 const btnGo = document.getElementById('btn-go');
 const content = document.getElementById('content');
 const image = document.getElementById('img');
 
-const fetchApi = (value) =>{
-
+const fetchApi = (value) => {
+    const result = fetch('https://rickandmortyapi.com/api/character')
 }
 
-btnGo.addEventListener('click', () =>{
-    
+btnGo.addEventListener('click', () => {
+
 })
