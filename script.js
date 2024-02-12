38ecd0e47f9b3a795c1e398daf66d7a0d93d8b69
José Rafael <rafaelmatiashue@gmail.com>
2024-02-12 10:35:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const characterId= document.getElementById('characterId');
+const btnGo = document.getElementById('btn-go');
+const content = document.getElementById('content');
+const image = document.getElementById('img');
+
+const fetchApi = (value) =>{
+
+}
+
+btnGo.addEventListener('click', () =>{
+    
+})
